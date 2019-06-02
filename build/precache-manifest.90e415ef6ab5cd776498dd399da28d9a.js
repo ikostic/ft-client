f78ec0cdfdbfa4b896fc8329259748dd6cb830a8
Igor Kostic <igorkostic@MacBook-Pro-60.local>
2019-06-02 22:10:38 +0200

    Added build

---
+++ build/precache-manifest.90e415ef6ab5cd776498dd399da28d9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2ea5476becb2f7b8bbe74ce16c4203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "607bf45c999838613c7c",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "7bf858adafa825e62fe1",
+    "url": "/static/css/main.59eec194.chunk.css"
+  },
+  {
+    "revision": "607bf45c999838613c7c",
+    "url": "/static/js/2.6710a8db.chunk.js"
+  },
+  {
+    "revision": "7bf858adafa825e62fe1",
+    "url": "/static/js/main.92d264b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0c2795492a1cfb02d98c991602ce6e67",
+    "url": "/static/media/building_800.0c279549.png"
+  }
+]);
